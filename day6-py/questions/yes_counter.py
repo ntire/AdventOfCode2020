8365594e07c51b4a97774f2b7956011738c42c62
Christian Heise <christian.heise@de.bosch.com>
2021-04-11 10:37:49 +0200

    Day6 - Part 2 (python)

--- day6-py/questions/yes_counter.py
+++ day6-py/questions/yes_counter.py
@@ -5,3 +5,21 @@ def count_unique_answers_of_anyone(all_answers):
         for item in answer:
             unique_answers.add(item)
     return len(unique_answers)
+
+def count_unique_answers_of_everyone(answer_group):
+    # initialize
+    counter = dict()
+    for letter in range(26):
+        counter[chr(ord('a') + letter)] = 0
+    
+    for single_person_answers in answer_group:
+        for answer in single_person_answers:
+            counter[answer] = counter.get(answer) + 1
+    
+    total_persons = len(answer_group)
+    everyone_counter = 0
+    for answer in counter.keys():
+        if counter.get(answer) == total_persons:
+            everyone_counter += 1
+
+    return everyone_counter
